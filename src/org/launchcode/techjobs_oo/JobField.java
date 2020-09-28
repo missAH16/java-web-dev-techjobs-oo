76dae12e755098148b6af1acf9abfdf861134979
missAH16 <missahiatt@gmail.com>
2020-09-27 23:12:08 -0500

    DRY code, working tests.

---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,44 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public abstract class JobField{
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String aValue) {
+        this();
+        this.value = aValue;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField that = (JobField) o;
+        return id == that.id &&
+                Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
+    public int getId() { return id; }
+
+    public String getValue() { return value; }
+
+    public void setValue(String value) { this.value = value; }
+}
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -2,37 +2,39 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
+public class PositionType extends JobField {
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
 
     public PositionType() {
-        id = nextId;
-        nextId++;
+//        id = nextId;
+//        nextId++;
     }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
+//        this();
+//        this.value = value;
     }
 
 
-    @Override
-    public String toString() { return  value; }
+//   @Override
+//    public String toString() {
+//    return  value; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PositionType that = (PositionType) o;
-        return id == that.id &&
-                Objects.equals(value, that.value); }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        PositionType that = (PositionType) o;
+//        return id == that.id &&
+//                Objects.equals(value, that.value); }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, value);
-    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, value);
+//    }
 
 
 
@@ -45,15 +47,15 @@ public class PositionType {
 
     // Getters and Setters:
 
-    public int getId() {
-        return id;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 }
