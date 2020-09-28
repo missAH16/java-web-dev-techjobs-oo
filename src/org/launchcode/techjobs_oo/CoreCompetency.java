4ea7188980883c2a9f3eba55f3e47a42c19340b4
missAH16 <missahiatt@gmail.com>
2020-09-28 18:13:46 -0500

    uncommented out

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -17,6 +17,7 @@ public class CoreCompetency extends JobField{
     public CoreCompetency(String value) {
         this();
         this.value = value;
+
     }
 
     // Custom toString, equals, and hashCode methods:
